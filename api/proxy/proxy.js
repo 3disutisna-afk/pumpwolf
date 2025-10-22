ddaf89d21ffe1b782e2ecb81b7bb6023e62d5e90
3disutisna-afk <3disutisna@gmail.com>
2025-10-22 08:39:35 +0700

    Create proxy.js

---
+++ api/proxy/proxy.js
@@ -0,0 +1,13 @@
+// api/proxy/proxy.js
+export default function handler(req, res) {
+  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+  const pathSegments = pathname.split('/').filter(Boolean);
+  if (pathSegments[0] === 'proxy' && pathSegments.length > 1) {
+    const targetPath = pathSegments.slice(1).join('/');
+    const targetUrl = `/api/${targetPath}`;
+    res.writeHead(302, { Location: targetUrl });
+    res.end();
+  } else {
+    res.status(404).json({ error: "Not found" });
+  }
+}
